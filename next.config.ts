de7f1e7a92463f45244242840b3052da12b94d79
Hoailp <phuochoaileqn@gmail.com>
2026-05-12 13:56:31 +0700

    fix: correct indentation in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    assetPrefix: 'https://sample-rewrite-eight.vercel.app',  
+  assetPrefix: 'https://sample-rewrite-eight.vercel.app',  
+  images: {
+    path: 'https://sample-rewrite-eight.vercel.app/_next/image',
+  },
 };
 
 export default nextConfig;
