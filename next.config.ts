d5010b36144e7c95441cfec651884c0056abbc70
Hoailp <phuochoaileqn@gmail.com>
2026-05-12 13:45:04 +0700

    fix: update assetPrefix in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    assetPrefix: 'https://sample-rewrite-rocvzzg7i-phuoc-hoais-projects.vercel.app',  
+    assetPrefix: 'https://sample-rewrite-eight.vercel.app',  
 };
 
 export default nextConfig;
